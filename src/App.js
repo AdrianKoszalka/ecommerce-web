5494584c39c821f297207b30c51686491e9d0db3
Adrian Koszalka <adriankoszalka96@gmail.com>
2022-04-12 22:28:28 +0200

    Connect React App with Django Backend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 function App() {
   return (
-      <div className='App'>
-        <Header />
-        <main className='py-3'>
-          <Container>
-            <HomePage />
-          </Container>
-        </main>
-        <Footer />
-      </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={ <HomePage /> } exact />
+            <Route path='/home' element={ <HomePage /> } />
+            <Route path='/product/:id' element={ <ProductPage /> } />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
